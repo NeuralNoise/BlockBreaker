c944a43c59e0543b687fb87f499368cdf1f8a860
AdrienGannerie <adrien.gannerie@gmail.com>
2016-06-08 15:29:55 +0200

    Ajout des scenes , netoyage des assets et importation des scripts de scenes.

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SceneLoader : MonoBehaviour
+{
+    public string Scene;
+
+    public void Load()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(Scene);
+    }
+}
