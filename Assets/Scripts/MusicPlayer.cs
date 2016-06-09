b9a79dda0d1eff7f467820ef17c3447ca4725267
AdrienGannerie <adrien.gannerie@gmail.com>
2016-06-09 17:41:21 +0200

    Avancé du 09/06/2016

---
+++ Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour
+{
+    static MusicPlayer instance = null;
+
+    void Awake()
+    {
+        if (instance != null)
+        {
+            Destroy(gameObject);
+            print("Destroy on load");
+        }
+        else
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+}
